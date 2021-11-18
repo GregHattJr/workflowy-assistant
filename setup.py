54069e4ca2d2815919b49a971f54a84fdb0a4bc8
Guido Knoop <guidoknoop@outlook.com>
2021-11-18 20:50:17 +0100

    New version

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+import setuptools
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 # This call to setup() does all the work
 setup(
     name="workflowy-assistant",
-    version="0.0.2",
+    version="0.0.3",
     description="Helps you automate your WorkFlowy.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,8 +20,11 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
     ],
     py_modules=["workflowy_assistant"],
     install_requires=["selenium", "webdriver_manager"],
     package_dir={'':'src'},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
 )
